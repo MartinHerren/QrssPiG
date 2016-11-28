25a37090beb62fbfe2f7b3bd58b7f048eeaeb11b
Martin Herren <sputnik@on-the-web.ch>
2016-11-28 17:47:52 +0100

    IQGen should now generate complex signals, to be checked with some viewer like RFAnalyzer

--- src/IQGen.cpp
+++ src/IQGen.cpp
@@ -1,38 +1,35 @@
+#include <complex>
 #include <iostream>
 
-#include <math.h>
-#include <stdlib.h>
+int S = 2000; // Sample rate
 
-int S = 1000000; // Sample rate
-
-inline double genI(double t) {
-	double v;
-	
-	//v = sin(2. * M_PI * 10 * t) + .7 * sin(2. * M_PI * 30 * t) + .5 * sin(2. * M_PI * 70 * t);
-	v = sin(2. * M_PI * 100 * t);
-	
-	// Hard clip result
-	if (v > 1.0) v = 1.0;
-	if (v < -1.0) v = -1.0;
-	
-	return v;
-}
-
-inline double genQ(double t) {
-	t;
-	return 0.;
+inline std::complex<double> genIQ(double f, double t) {
+	double wt = 2. * M_PI * f * t;
+	return std::complex<double>(cos(wt), sin(wt));
 }
 
 int main(int argc, char *argv[]) {
-	srand((unsigned) time(NULL));
+	bool unsignedIQ = false;
+	int numSeconds = 1;
+
+	//srand((unsigned) time(NULL));
 	
-	int numSeconds = 10;
 	
 	for (double t = 0.; t < numSeconds; t += 1./S) {
 		//double r = (double)rand()/RAND_MAX - .5;
 		
-		unsigned char i =  ((unsigned char)trunc(255./2 * (1 + genI(t)))) & 0xff;
-		unsigned char q =  ((unsigned char)trunc(255./2 * (1 + genQ(t)))) & 0xff;
+		//std::complex<double> iq = genIQ(100, t) + genIQ(150, t) + genIQ(-200, t);
+		std::complex<double> iq = .1 * genIQ(100, t);
+
+		// Hard clip magnitude to [-1,1] interval
+		double iqAbs = abs(iq);
+		if (iqAbs > 1) iq /= iqAbs;
+
+		// Shift I/Q to [0,2] interval for unsigned output
+		if (unsignedIQ) iq += std::complex<double>(1., 1.);
+		
+		unsigned char i = ((unsigned char)trunc(127 * iq.real())) & 0xff;
+		unsigned char q = ((unsigned char)trunc(127 * iq.imag())) & 0xff;
 		
 		std::cout << i << q;
 	}
