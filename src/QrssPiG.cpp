511c41b2ba5ccea2b068d942250bef499c146c10
Martin <sputnik@on-the-web.ch>
2017-04-30 10:41:17 +0200

    Remove duplicated constructor

--- src/QrssPiG.cpp
+++ src/QrssPiG.cpp
@@ -17,6 +17,8 @@ QrssPiG::QrssPiG(bool unsignedIQ, int sampleRate, int N) :
 QrssPiG::QrssPiG(const std::string &configFile) {
 	std::cout << "piggy from config: " << configFile << std::endl;
 	YAML::Node config = YAML::LoadFile(configFile);
+
+	_init();
 }
 
 QrssPiG::~QrssPiG() {
