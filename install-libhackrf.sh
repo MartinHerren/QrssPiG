4f7269d7c60a9112670e52869576fa7e8d5c339b
Martin <sputnik@on-the-web.ch>
2017-08-30 14:34:29 +0200

    Attempt to install libHackRF v2015.07.2 in Travis

--- install-libhackrf.sh
+++ install-libhackrf.sh
@@ -2,4 +2,4 @@
 set -ex
 wget https://github.com/mossmann/hackrf/archive/v2015.07.2.tar.gz
 tar -xzvf v2015.07.2.tar.gz
-mkdir -p hackrf-2015.07.2/host/build && cd hackrf-2015.07.2/host/build && cmake ../ -DCMAKE_INSTALL_PREFIX=/usr && make && sudo make install && sudo ldconfig
+mkdir -p hackrf-2015.07.2/host/build && cd hackrf-2015.07.2/host/build && cmake ../ -DCMAKE_INSTALL_PREFIX=/usr -DINSTALL_UDEV_RULES=ON && make && sudo make install && sudo ldconfig
