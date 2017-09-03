f172d9d4582513264ccccc954a9597e8e2e8d732
HB9FXX - Martin Herren <cq@hb9fxx.ch>
2017-09-03 20:51:45 +0000

    Improvements on downsampling tests.

--- src/QGDownSampler.cpp
+++ src/QGDownSampler.cpp
@@ -1,78 +1,73 @@
 #include "QGDownSampler.h"
 
 #include <chrono>
-#include <complex>
 #include <iostream>
 #include <liquid/liquid.h>
+#include <math.h>
 #include <rtf_common.h>
 #include <rtfilter.h>
 #include <stdexcept>
 
-QGDownSampler::QGDownSampler(float rate): _rate(rate) {
+QGDownSampler::QGDownSampler(float rate, unsigned int cs): _rate(rate), _cs(cs) {
+	_inAllocated = new std::complex<float>[_cs+15]; // +15 to be able to enforce 16byte alignement
+	_outAllocated = new std::complex<float>[(int)ceil(1.1/_rate*_cs)+15]; // 10% safe for flexible filter output
+	_in = (std::complex<float>*)(((uintptr_t)_inAllocated + 15) & ~(uintptr_t)0xf);
+	_out = (std::complex<float>*)(((uintptr_t)_outAllocated + 15) & ~(uintptr_t)0xf);
 }
 
 QGDownSampler::~QGDownSampler() {
+	delete [] _inAllocated;
+	delete [] _outAllocated;
 }
 
 void QGDownSampler::testRTFilter() {
-	std::complex<float> *inP = new std::complex<float>[1024+15];
-	std::complex<float> *outP = new std::complex<float>[1024+15];
-	std::complex<float> *in = (std::complex<float>*)(((uintptr_t)inP + 15) & ~(uintptr_t)0xf);
-	std::complex<float> *out = (std::complex<float>*)(((uintptr_t)outP + 15) & ~(uintptr_t)0xf);
-
-	std::cout << "Rate: " << _rate << std::endl;
-
 	hfilter f = rtf_create_downsampler(1, RTF_CFLOAT, _rate);
 
-	for (int chunkSize = 1; chunkSize <= 128; chunkSize *= 2) {
+	for (unsigned int chunkSize = 1; chunkSize <= _cs; chunkSize *= 2) {
 		using namespace std::chrono;
 		milliseconds start = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
 
 		int it = 0;
 		int ot = 0;
+		int mo = 0;
 
-		for (int i = 0; i < (1000*1000)/chunkSize; i++) {
+		for (unsigned int i = 0; i < (8*1000*1000)/chunkSize; i++) {
 			it += chunkSize;
-			ot += rtf_filter(f, in, out, chunkSize);
+			int o = rtf_filter(f, _in, _out, chunkSize);
+			ot += o;
+			if (o > mo) mo = o;
 		}
 		milliseconds stop = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
 		int t = stop.count() - start.count();
 
-		std::cout << "cs: " << chunkSize << " " << it << " " << ot << " " << (it/ot) << " " << t << "ms " << (1000./t) << "MS/s" << std::endl;
+		std::cout << "cs: " << chunkSize << " " << it << " " << ot << " " << ((float)it/ot) << " " << t << "ms " << (8*1000./t) << "MS/s " << mo << std::endl;
 	}
 
-
 	rtf_destroy_filter(f);
-
-	delete [] inP;
-	delete [] outP;
 }
 
 void QGDownSampler::testLiquidDsp() {
-	std::complex<float> *inP = new std::complex<float>[1024+15];
-	std::complex<float> *outP = new std::complex<float>[1024+15];
-	std::complex<float> *in = (std::complex<float>*)(((uintptr_t)inP + 15) & ~(uintptr_t)0xf);
-	std::complex<float> *out = (std::complex<float>*)(((uintptr_t)outP + 15) & ~(uintptr_t)0xf);
-
 	resamp_crcf q = resamp_crcf_create(1./_rate, 13, .45, 60., 32);
 
-	for (int chunkSize = 1; chunkSize <= 128; chunkSize *= 2) {
+	for (unsigned int chunkSize = 1; chunkSize <= _cs; chunkSize *= 2) {
 		using namespace std::chrono;
 		milliseconds start = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
 
 		int it = 0;
 		int ot = 0;
+		unsigned int mo = 0;
 		
-		for (int i = 0; i < (1000*1000)/chunkSize; i++) {
+		for (unsigned int i = 0; i < (8*1000*1000)/chunkSize; i++) {
 			it += chunkSize;
 			unsigned int o;
-			resamp_crcf_execute_block(q, in, chunkSize, out, &o);
+			resamp_crcf_execute_block(q, _in, chunkSize, _out, &o);
 			ot += o;
+			if (o > mo) mo = o;
 		}
 		milliseconds stop = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
 		int t = stop.count() - start.count();
 
-		std::cout << "cs: " << chunkSize << " " << it << " " << ot << " " << (it/ot) << " " << t << "ms " << (1000./t) << "MS/s" << std::endl;
+		std::cout << "cs: " << chunkSize << " " << it << " " << ot << " " << ((float)it/ot) << " " << t << "ms " << (8*1000./t) << "MS/s " << mo << std::endl;
 	}
 
 	resamp_crcf_destroy(q);
--- src/QGDownSampler.h
+++ src/QGDownSampler.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <complex>
+
 class QGDownSampler {
 public:
-	QGDownSampler(float rate);
+	QGDownSampler(float rate, unsigned int cs);
 	~QGDownSampler();
 
 	void testRTFilter();
@@ -10,4 +12,11 @@ public:
 
 private:
 	float _rate;
+	unsigned int _cs;
+
+	// internal
+	std::complex<float> *_inAllocated;
+	std::complex<float> *_outAllocated;
+	std::complex<float> *_in;
+	std::complex<float> *_out;
 };
