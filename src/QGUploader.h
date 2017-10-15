d10323f949768ec6424b3d411812695b59acd429
Martin <sputnik@on-the-web.ch>
2017-10-15 23:57:13 +0200

    Initial ftp support

--- src/QGUploader.h
+++ src/QGUploader.h
@@ -1,18 +1,25 @@
 #pragma once
 
 #include <string>
+#include <yaml-cpp/yaml.h>
 
 class QGUploader {
 protected:
-	QGUploader() {};
+	QGUploader() {}; // TODO Keep until SCP/LocalUploader migrated
+	QGUploader(const YAML::Node &config);
 
 public:
 	virtual ~QGUploader() {};
 
 	void push(const std::string &fileName, const char *data, int dataSize, bool wait = false);
 
+	static QGUploader *CreateUploader(const YAML::Node &config);
+
 private:
 	void _pushThread(std::string fileName, const char *data, int dataSize);
 
 	virtual void _pushThreadImpl(const std::string &fileName, const char *data, int dataSize, std::string &uri) = 0;
+
+protected:
+	bool _verbose;
 };
---
+++ src/QGUploaderFTP.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+
+#include "QGUploader.h"
+
+class QGUploaderFTP: public QGUploader {
+public:
+	QGUploaderFTP(const YAML::Node &config);
+	~QGUploaderFTP();
+
+private:
+	struct FTPUploadData { const char *readptr; size_t sizeleft; };
+
+	void _pushThreadImpl(const std::string &fileName, const char *data, int dataSize, std::string &uri);
+
+	static size_t cb(void *ptr, size_t size, size_t nmemb, void *userp);
+
+	std::string _host;
+	int _port;
+	std::string _user;
+	std::string _password;
+	std::string _dir;
+	int _fileMode;
+};
