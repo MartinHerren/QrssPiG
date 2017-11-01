664f068d576ebb7040b50a3aec3924faaca0eeb9
Martin <sputnik@on-the-web.ch>
2017-11-01 21:22:36 +0100

    Initial tests acquiring from hackrf

--- src/QGInputHackRF.cpp
+++ src/QGInputHackRF.cpp
@@ -54,15 +54,67 @@ QGInputHackRF::~QGInputHackRF() {
 }
 
 void QGInputHackRF::deviceList() {
+	int r = hackrf_init();
+
+	if (r != HACKRF_SUCCESS) {
+		throw std::runtime_error(std::string("HackRF init returned ") + std::to_string(r));
+	}
+
 	hackrf_device_list_t *l = hackrf_device_list();
 
 	std::cout << "Devices: " << l->devicecount << std::endl;
 
 	hackrf_device_list_free(l);
+
+	hackrf_exit();
 }
 
 void QGInputHackRF::run(std::function<void(std::complex<float>)>cb) {
+	_running.lock(); // lock to running state.
+	_cb = cb;
+
+	int r = hackrf_start_rx(_device, async, this);
+
+	if (r != HACKRF_SUCCESS) {
+		throw std::runtime_error(std::string("HackRF run failed: ") + std::to_string(r));
+	}
+
+	_running.lock(); // block until lock as been freed by stop()
+	_running.unlock(); // Cleanup
+
+	return;
 }
 
 void QGInputHackRF::stop() {
+	if (!_running.try_lock()) {
+		int r = hackrf_stop_rx(_device);
+
+		if (r != HACKRF_SUCCESS) {
+			throw std::runtime_error(std::string("HackRF stop failed: ") + std::to_string(r));
+		}
+	}
+	_running.unlock();// Enable run() to return, or release the lock we just gained if it was not running
+}
+
+void QGInputHackRF::process(uint8_t *buf, int len) {
+	std::cout << "cb" << std::endl;
+	// S8 IQ data
+	signed char i, q;
+
+	for (int j = 0; j < len;) {
+		i = buf[j++];
+		q = buf[j++];
+		_cb(std::complex<float>(i / 128., q / 128.));
+	}
+	std::cout << "cb done" << std::endl;
+}
+
+int QGInputHackRF::async(hackrf_transfer* transfer) {
+	try {
+		static_cast<QGInputHackRF *>(transfer->rx_ctx)->process(transfer->buffer, transfer->valid_length);
+	} catch (const std::exception &e) {
+		// TODO reset device  if error is from rtl device read and ignore if from fft ?
+	}
+
+	return HACKRF_SUCCESS;
 }
--- src/QGInputHackRF.h
+++ src/QGInputHackRF.h
@@ -2,6 +2,8 @@
 
 #include "QGInputDevice.h"
 
+#include <mutex>
+
 #include <libhackrf/hackrf.h>
 
 class QGInputHackRF: public QGInputDevice {
@@ -14,6 +16,13 @@ public:
 	void run(std::function<void(std::complex<float>)>cb);
 	void stop();
 
+	void process(uint8_t *buf, int len);
+
+	static int async(hackrf_transfer* transfer);
+
 private:
 	hackrf_device *_device;
+	std::function<void(std::complex<float>)>_cb;
+
+	std::mutex _running;
 };
