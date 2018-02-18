a089b8822b64251746bd82bc1375f7ce43819b0c
Martin Herren <martin.herren@gmail.com>
2018-02-18 18:39:00 +0100

    Check if hackrf_set_antenna_enable available, conditional compilation upon result

--- cmake/Modules/FindLibHackRF.cmake
+++ cmake/Modules/FindLibHackRF.cmake
@@ -19,3 +19,4 @@ set(CMAKE_REQUIRED_INCLUDES ${LibHackRF_INCLUDE_DIRS})
 set(CMAKE_REQUIRED_LIBRARIES ${LibHackRF_LIBRARIES})
 
 CHECK_SYMBOL_EXISTS(hackrf_device_list libhackrf/hackrf.h LibHackRF_HAS_hackrf_device_list)
+CHECK_SYMBOL_EXISTS(hackrf_set_antenna_enable libhackrf/hackrf.h LibHackRF_HAS_hackrf_set_antenna_enable)
